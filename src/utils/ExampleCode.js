d370152fcf20b92e43238ac99aa808039dbebc10
William Hawker <williamhawker@benefex.co.uk>
2018-04-25 22:50:31 +0100

    Add a Edit on StackBlitz banner

--- src/utils/ExampleCode.js
+++ src/utils/ExampleCode.js
@@ -6,9 +6,14 @@ import 'prismjs/themes/prism.css';
 
 export default ({ name, children }) => (
   <div>
+    <a className="github-fork-ribbon right-top"
+       href={`https://stackblitz.com/github/whawker/react-jsx-highcharts-examples?file=src%2F${name}%2F${name}.js`}
+       data-ribbon="Edit on StackBlitz"
+       title="Edit on StackBlitz">Edit on StackBlitz</a>
     <pre>
       <PrismCode className="language-jsx">{children}</PrismCode>
     </pre>
-    <a href={`https://github.com/whawker/react-jsx-highcharts-examples/blob/master/src/${name}/${name}.js`} className="btn btn-link">See full example code</a>
+    <a href={`https://stackblitz.com/github/whawker/react-jsx-highcharts-examples?file=src%2F${name}%2F${name}.js`}
+       className="btn btn-link">See full example code</a>
   </div>
 );
