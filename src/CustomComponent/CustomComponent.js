f832a3d14eda747bc651d1a4e601bc6eecbefc25
Will Hawker <w.d.hawker@gmail.com>
2019-10-02 12:24:32 +0100

    fixup! Update CustomComponent example to use hooks

--- src/CustomComponent/CustomComponent.js
+++ src/CustomComponent/CustomComponent.js
@@ -52,13 +52,6 @@ class App extends Component {
           </Navigator>
         </HighchartsStockChart>
 
-        <div className="alert alert-info" role="alert">
-          <p>
-            This custom component is now available as an add-on NPM package:
-            <a href="https://npmjs.com/package/react-jsx-highstock-datepickers" target="_blank" rel="noopener noreferrer"><code>react-jsx-highstock-datepickers</code></a>
-          </p>
-        </div>
-
         <ExampleCode name="CustomComponent">{code}</ExampleCode>
       </div>
     );
