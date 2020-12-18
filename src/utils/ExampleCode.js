51a5da69a18bf222a83e27b1f58fb3114315007d
Antti Lehto <anajavi@users.noreply.github.com>
2020-12-18 22:56:56 +0200

    Switch to react-syntax-highlighter

--- src/utils/ExampleCode.js
+++ src/utils/ExampleCode.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import 'prismjs';
-import 'prismjs/components/prism-jsx';
-import { PrismCode } from 'react-prism';
-import 'prismjs/themes/prism.css';
+import { Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 
 export default ({ name, children }) => (
   <div>
@@ -10,9 +7,7 @@ export default ({ name, children }) => (
        href={`https://stackblitz.com/github/whawker/react-jsx-highcharts-examples?file=src%2F${name}%2F${name}.js`}
        data-ribbon="Edit on StackBlitz"
        title="Edit on StackBlitz">Edit on StackBlitz</a>
-    <pre>
-      <PrismCode className="language-jsx">{children}</PrismCode>
-    </pre>
+    <SyntaxHighlighter language="jsx">{children}</SyntaxHighlighter>
     <a href={`https://stackblitz.com/github/whawker/react-jsx-highcharts-examples?file=src%2F${name}%2F${name}.js`}
        className="btn btn-link">See full example code</a>
   </div>
