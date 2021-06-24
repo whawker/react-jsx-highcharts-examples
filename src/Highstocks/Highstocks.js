b7c59f407a33a92c0e8ec49b4709ac0e85bcf2f5
Antti Lehto <anajavi@users.noreply.github.com>
2021-06-24 10:08:30 +0300

    Modify highstock example to functional component

--- src/Highstocks/Highstocks.js
+++ src/Highstocks/Highstocks.js
@@ -1,75 +1,79 @@
-import React, { Component } from 'react';
-import Highcharts from 'highcharts/highstock';
+import React, { Component } from "react";
+import Highcharts from "highcharts/highstock";
 import {
-  HighchartsStockChart, Chart, HighchartsProvider, XAxis, YAxis, Title, Legend,
-  AreaSplineSeries, SplineSeries, Navigator, RangeSelector, Tooltip
-} from 'react-jsx-highstock';
-import ExampleCode from '../utils/ExampleCode';
-import code from './exampleCode';
-import { createRandomData } from '../utils/data-helpers';
+  HighchartsStockChart,
+  Chart,
+  HighchartsProvider,
+  XAxis,
+  YAxis,
+  Title,
+  Legend,
+  AreaSplineSeries,
+  SplineSeries,
+  Navigator,
+  RangeSelector,
+  Tooltip,
+} from "react-jsx-highstock";
+import ExampleCode from "../utils/ExampleCode";
+import code from "./exampleCode";
+import { createRandomData } from "../utils/data-helpers";
 
-class App extends Component {
+const now = Date.now();
+const data1 = createRandomData(now, 1e7, 500);
+const data2 = createRandomData(now, 1e7, 500);
 
-  constructor (props) {
-    super(props);
+const App = () => {
+  return (
+    <div className="app">
+      <HighchartsProvider Highcharts={Highcharts}>
+        <HighchartsStockChart>
+          <Chart zoomType="x" />
 
-    const now = Date.now();
-    this.state = {
-      data1: createRandomData(now, 1e7, 500),
-      data2: createRandomData(now, 1e7, 500)
-    };
-  }
+          <Title>Highstocks Example</Title>
 
-  render() {
-    const { data1, data2 } = this.state;
+          <Legend>
+            <Legend.Title>Key</Legend.Title>
+          </Legend>
 
-    return (
-      <div className="app">
-        <HighchartsProvider Highcharts={Highcharts}>
-          <HighchartsStockChart>
-            <Chart zoomType="x" />
+          <Tooltip />
 
-            <Title>Highstocks Example</Title>
+          <XAxis>
+            <XAxis.Title>Time</XAxis.Title>
+          </XAxis>
 
-            <Legend>
-              <Legend.Title>Key</Legend.Title>
-            </Legend>
+          <YAxis>
+            <YAxis.Title>Price</YAxis.Title>
+            <AreaSplineSeries id="profit" name="Profit" data={data1} />
+          </YAxis>
 
-            <Tooltip />
+          <YAxis opposite>
+            <YAxis.Title>Social Buzz</YAxis.Title>
+            <SplineSeries id="twitter" name="Twitter mentions" data={data2} />
+          </YAxis>
 
-            <XAxis>
-              <XAxis.Title>Time</XAxis.Title>
-            </XAxis>
+          <RangeSelector selected={1}>
+            <RangeSelector.Button count={1} type="day">
+              1d
+            </RangeSelector.Button>
+            <RangeSelector.Button count={7} type="day">
+              7d
+            </RangeSelector.Button>
+            <RangeSelector.Button count={1} type="month">
+              1m
+            </RangeSelector.Button>
+            <RangeSelector.Button type="all">All</RangeSelector.Button>
+            <RangeSelector.Input boxBorderColor="#7cb5ec" />
+          </RangeSelector>
 
-            <YAxis>
-              <YAxis.Title>Price</YAxis.Title>
-              <AreaSplineSeries id="profit" name="Profit" data={data1} />
-            </YAxis>
-
-            <YAxis opposite>
-              <YAxis.Title>Social Buzz</YAxis.Title>
-              <SplineSeries id="twitter" name="Twitter mentions" data={data2} />
-            </YAxis>
-
-            <RangeSelector selected={1}>
-              <RangeSelector.Button count={1} type="day">1d</RangeSelector.Button>
-              <RangeSelector.Button count={7} type="day">7d</RangeSelector.Button>
-              <RangeSelector.Button count={1} type="month">1m</RangeSelector.Button>
-              <RangeSelector.Button type="all">All</RangeSelector.Button>
-              <RangeSelector.Input boxBorderColor="#7cb5ec" />
-            </RangeSelector>
-
-            <Navigator>
-              <Navigator.Series seriesId="profit" />
-              <Navigator.Series seriesId="twitter" />
-            </Navigator>
-          </HighchartsStockChart>
-        </HighchartsProvider>
-        <ExampleCode name="Highstocks">{code}</ExampleCode>
-
-      </div>
-    );
-  }
-}
+          <Navigator>
+            <Navigator.Series seriesId="profit" />
+            <Navigator.Series seriesId="twitter" />
+          </Navigator>
+        </HighchartsStockChart>
+      </HighchartsProvider>
+      <ExampleCode name="Highstocks">{code}</ExampleCode>
+    </div>
+  );
+};
 
 export default App;
