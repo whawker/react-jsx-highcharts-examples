e14a0a05be29b4295eac3e7d99d9285015a34315
Antti Lehto <anajavi@users.noreply.github.com>
2026-09-08 12:39:48 +0300

    Convert 3DChart from class to function

--- src/3DChart/3DChart.tsx
+++ src/3DChart/3DChart.tsx
@@ -1,96 +1,95 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Highcharts from 'highcharts';
 import 'highcharts/highcharts-3d';
 import {
-  Highcharts3dChart, Chart, HighchartsProvider, XAxis, YAxis, ZAxis, Title, Subtitle, ScatterSeries
+  Highcharts3dChart,
+  Chart,
+  HighchartsProvider,
+  XAxis,
+  YAxis,
+  ZAxis,
+  Title,
+  Subtitle,
+  ScatterSeries
 } from 'react-jsx-highcharts';
 import zones from './zones';
 import ExampleCode from '../utils/ExampleCode';
 import code from './exampleCode';
 const radian = Math.PI / 180;
 
+const numRows = 25;
+const data: [number, number, number][] = [];
 
-class ThreeDChart extends Component {
+const d = 360 / numRows;
+const step = d * radian;
 
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      numRows: 25,
-      beta: 22
-    };
-  }
-
-  componentWillMount () {
-    const { numRows } = this.state;
-
-    const data = [];
-    const d = 360 / numRows;
-    const step = d * radian;
-
-    for (let i = 0; i < numRows; i++) {
-      const iCos = Math.cos(i * step);
-      for (let j = 0; j < numRows; j++) {
-        const value = iCos * Math.cos(j * step);
-        data.push([ i, value, j ]);
-      }
-    }
-
-    this.setState({
-      data
-    });
-  }
-
-  handleSliderChange = e => {
-    this.setState({
-      beta: e.target.value
-    });
-  }
-
-  render () {
-    const { numRows, data, beta } = this.state;
-    if (!data) return null;
-
-    const plotOptions = {
-      scatter: {
-        marker: {
-          states: {
-            hover: { enabled: false },
-            select: { enabled: false }
-          }
-        }
-      }
-    };
-
-    return (
-      <div className="app">
-        <HighchartsProvider Highcharts={Highcharts}>
-          <Highcharts3dChart alpha={30} beta={beta} depth={300} viewDistance={0} plotOptions={plotOptions}>
-            <Chart />
-
-            <Title>3D Scatter Chart</Title>
-
-            <Subtitle>Plotting cos(x) * cos(y) (with zones for colours)</Subtitle>
-
-            <XAxis min={0} max={numRows} labels={{ enabled: false }} />
-
-            <YAxis min={-2} max={2} labels={{ enabled: false }} />
-
-            <ZAxis min={0} max={numRows} labels={{ enabled: false }}>
-              <ScatterSeries data={data} zones={zones} />
-            </ZAxis>
-          </Highcharts3dChart>
-        </HighchartsProvider>
-        <div>
-          <label htmlFor="beta">Beta Angle</label>
-          <input id="beta" type="range" min="0" max="45" step="1" value={beta} onChange={this.handleSliderChange} />
-          <span>{beta}</span>
-        </div>
-
-        <ExampleCode name="3DChart">{code}</ExampleCode>
-      </div>
-    );
+for (let i = 0; i < numRows; i++) {
+  const iCos = Math.cos(i * step);
+  for (let j = 0; j < numRows; j++) {
+    const value = iCos * Math.cos(j * step);
+    data.push([i, value, j]);
   }
 }
+const ThreeDChart = () => {
+  const [beta, setBeta] = useState(22);
+
+  const handleSliderChange = e => {
+    setBeta(e.target.value);
+  };
+
+  const plotOptions = {
+    scatter: {
+      marker: {
+        states: {
+          hover: { enabled: false },
+          select: { enabled: false }
+        }
+      }
+    }
+  };
+
+  return (
+    <div className="app">
+      <HighchartsProvider Highcharts={Highcharts}>
+        <Highcharts3dChart
+          alpha={30}
+          beta={beta}
+          depth={300}
+          viewDistance={0}
+          plotOptions={plotOptions}
+        >
+          <Chart />
+
+          <Title>3D Scatter Chart</Title>
+
+          <Subtitle>Plotting cos(x) * cos(y) (with zones for colours)</Subtitle>
+
+          <XAxis min={0} max={numRows} labels={{ enabled: false }} />
+
+          <YAxis min={-2} max={2} labels={{ enabled: false }} />
+
+          <ZAxis min={0} max={numRows} labels={{ enabled: false }}>
+            <ScatterSeries data={data} zones={zones} />
+          </ZAxis>
+        </Highcharts3dChart>
+      </HighchartsProvider>
+      <div>
+        <label htmlFor="beta">Beta Angle</label>
+        <input
+          id="beta"
+          type="range"
+          min="0"
+          max="45"
+          step="1"
+          value={beta}
+          onChange={handleSliderChange}
+        />
+        <span>{beta}</span>
+      </div>
+
+      <ExampleCode name="3DChart">{code}</ExampleCode>
+    </div>
+  );
+};
 
 export default ThreeDChart;
